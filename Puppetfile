99ce88da961a8d1767fbfabbf85d2ed65fe83804
Joshua Hoblitt <jhoblitt@cpan.org>
2015-03-30 21:32:12 -0700

    (DM-2383) migrate package deps to puppet-lsststack

--- Puppetfile
+++ Puppetfile
@@ -6,3 +6,5 @@ mod 'stahnma/epel'
 mod 'petems/swap_file', '~> 1.0.0'
 mod 'jhoblitt/sysstat'
 mod 'maestrodev/wget', '~> 1.5.7'
+
+mod 'jhoblitt/lsststack', :git => 'https://github.com/lsst-sqre/puppet-lsststack.git'
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,9 @@ include ::stdlib
 include ::augeas
 include ::sysstat
 include ::wget
+include ::lsststack
+
+Class['lsststack'] -> File['newinstall.sh']
 
 case $::osfamily {
   'Debian': {
@@ -9,39 +12,6 @@ case $::osfamily {
       'screen',
       'tree',
       'vim'
-    ]
-
-    $pkg_list = [
-      # needed for newinstall.sh
-      'make',
-      # list from https://confluence.lsstcorp.org/display/LSWUG/Prerequisites
-      'bison',
-      'curl',
-      'ca-certificates', # needed by curl on ubuntu
-      'flex',
-      'g++',
-      'git',
-      'libbz2-dev',
-      'libreadline6-dev',
-      'libx11-dev',
-      'libxt-dev',
-      'm4',
-      'zlib1g-dev',
-      # needed for shapelet tests
-      'libxrender1',
-      'libfontconfig1',
-      # needed by lua
-      'libncurses5-dev',
-      # needed for xrootd build
-      'cmake',
-      # needed for mysqlproxy
-      'libglib2.0-dev',
-      # needed to build zookeeper
-      'openjdk-7-jre',
-      # needed to build git
-      'gettext',
-      'libcurl4-openssl-dev',
-      'perl-modules',
     ]
   }
   'RedHat': {
@@ -53,47 +23,10 @@ case $::osfamily {
       'tree',
       'vim-enhanced'
     ]
-
-    $pkg_list = [
-      'bison',
-      'curl',
-      'blas',
-      'bzip2-devel',
-      'bzip2', # needed on el7 -- pulled in by bzip2-devel on el6?
-      'flex',
-      'fontconfig',
-      'freetype-devel',
-      'gcc-c++',
-      'gcc-gfortran',
-      'git', # needed on el6, in @core for others?
-      'libuuid-devel',
-      'libXext',
-      'libXrender',
-      'libXt-devel',
-      'make',
-      'openssl-devel',
-      'patch',
-      'perl',
-      'readline-devel',
-      'zlib-devel',
-      # needed by lua
-      'ncurses-devel',
-      # needed for xrootd build
-      'cmake',
-      # needed for mysqlproxy
-      'glib2-devel',
-      # needed to build zookeeper
-      'java-1.7.0-openjdk',
-      # needed to build git
-      'gettext',
-      'libcurl-devel',
-      'perl-ExtUtils-MakeMaker',
-    ]
   }
   default: { fail() }
 }
 
-package { $pkg_list: }
 package { $convience_pkgs: }
 
 $memoryrequired = to_bytes('16 GB')
@@ -144,7 +77,7 @@ file { 'stack':
   group   => $stack_group,
   mode    => '0755',
   path    => $stack_path,
-  require => [Class['swap_file'], Package[$pkg_list]],
+  require => Class['swap_file'],
 }
 
 wget::fetch { 'newinstall.sh':
